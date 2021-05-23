dbc3a7268bde3278a343f00d7f97f407da0cbe76
RobinLe <web@robinle.de>
2021-05-23 15:38:56 +0200

    New template file path scheme, lib header template
    Templates are now in "template" subfolder;
    Template for lib headers added

---
+++ template/libs/template.hpp
@@ -0,0 +1,43 @@
+/***************************************************************************************************
+ FILE:	template.hpp
+ LIB:	template.lib
+ DESCR:	Template for new library header files
+***************************************************************************************************/
+
+
+#pragma once
+#ifndef ROBINLE_TEMPLATE
+#define ROBINLE_TEMPLATE
+
+
+
+
+
+//==================================================================================================
+// FORWARD DECLARATIONS
+
+//--------------------------------------------------------------------------------------------------
+// <headername>
+// forward declarations
+
+
+// #includes
+
+
+
+//==================================================================================================
+// DECLARATION
+namespace rl
+{
+	
+	// declarations
+	
+}
+
+
+
+
+
+// #undef foward declared definitions
+
+#endif // ROBINLE_TEMPLATE

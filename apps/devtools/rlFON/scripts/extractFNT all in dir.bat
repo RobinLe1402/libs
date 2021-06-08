f5a297271020e39c4bb7d7abbe954e4e651b9f42
RobinLe <web@robinle.de>
2021-06-08 17:45:37 +0200

    devtools/rlFON: Added "extractFNT all in dir.bat"

---
+++ apps/devtools/rlFON/scripts/extractFNT all in dir.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+REM Thie script tries to every FONT resource of every .fon file in a directory
+
+IF NOT EXIST ".\extractFNT.exe" GOTO :NOEXE
+
+ECHO Input folder:
+SET /P "sSRC="
+SET sSRC=%sSRC:"=%
+
+ECHO.
+
+ECHO Output folder:
+SET /P "sDEST="
+SET sDEST=%sDEST:"=%
+
+FOR /R "%sSRC%" %%f IN (*.fon) DO (
+ECHO.
+ECHO.
+ECHO.
+ECHO File: "%%~nxf"
+extractFNT "%%f" * "%sDEST%"
+)
+
+PAUSE
+EXIT
+
+
+:NOEXE
+ECHO "extractFNT.exe" not found.
+PAUSE
+EXIT

c3624eef1f47272a174b1d12db197062b4c0b0ee
RobinLe <web@robinle.de>
2021-12-12 14:35:35 +0100

    comps/unicode.cpp/hpp: UTF-16 functions
    Added UTF-16-related functions to the rl::UTF16 namespace,
    marked all functions in unicode.cpp/hpp as noexcept

--- comps/include/rl/unicode.hpp
+++ comps/include/rl/unicode.hpp
@@ -11,6 +11,12 @@
 
 
 
+#if __cplusplus > 201703L
+#include <memory>
+#endif // C++17
+
+
+
 
 
 //==================================================================================================
@@ -23,12 +29,67 @@ namespace rl
 		/// <summary>
 		/// Is a Unicode character located inside of a private use area?
 		/// </summary>
-		bool IsPrivateUse(char32_t ch);
+		bool IsPrivateUse(char32_t ch) noexcept;
 
 		/// <summary>
 		/// Is a value a noncharacter in Unicode?
 		/// </summary>
-		bool IsNoncharacter(char32_t ch);
+		bool IsNoncharacter(char32_t ch) noexcept;
+
+	}
+
+	namespace UTF16
+	{
+
+		/// <summary>
+		/// Is a UTF-16 codeunit a high surrogate (the first part of a surrogate pair)?
+		/// </summary>
+		inline constexpr bool IsHighSurrogate(char16_t ch) noexcept
+		{
+			return ((ch & 0xFC00) == 0xD800);
+		}
+
+		/// <summary>
+		/// Is a UTF-16 codeunit a low surrogate (the second part of a surrogate pair)?
+		/// </summary>
+		inline constexpr bool IsLowSurrogate(char16_t ch) noexcept
+		{
+			return ((ch & 0xFC00) == 0xDC00);
+		}
+
+		/// <summary>
+		/// Decode a UTF-16 character<para />
+		/// <b>Warning:</b> Doesn't check if the codeunits are actually surrogates
+		/// </summary>
+		char32_t Decode(char16_t hi, char16_t lo) noexcept;
+
+
+
+#if __cplusplus > 201703L
+
+		/// <summary>
+		/// Decode a UTF-16 string to UTF-32<para />
+		/// Throws an <c>std::exception</c> if the input string is not valid
+		/// </summary>
+		/// <param name="len">
+		/// | If not <c>nullptr</c>, the pointed to variable receives the length, in characters, of
+		/// the decoded string (without the terminating zero)
+		/// </param>
+		std::shared_ptr<char32_t[]> DecodeString(const char16_t* sz, size_t* len = nullptr);
+
+#ifdef _WIN32
+		/// <summary>
+		/// Decode a wide string to UTF-32<para />
+		/// Throws an <c>std::exception</c> if the input string is not valid
+		/// </summary>
+		/// <param name="len">= The length, in characters, of the decoded string</param>
+		inline auto DecodeString(const wchar_t* sz, size_t* len = nullptr)
+		{
+			return DecodeString((const char16_t*)sz, len);
+		}
+#endif // _WIN32
+
+#endif // C++17
 
 	}
 }
